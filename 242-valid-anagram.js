fa09fef30eb8cfdd5b7d2fd3248c12241cc8f22b
vargasjuanibarra <vargasjuanibarra@gmail.com>
2024-05-15 09:15:48 -0400

    Solution for 242. Valid Anagram

---
+++ 242-valid-anagram.js
@@ -0,0 +1,21 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let countS = {};
+    let countT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        countS[s[i]] = (countS[s[i]] || 0) + 1;
+        countT[t[i]] = (countT[t[i]] || 0) + 1;
+    }
+
+    for (let c in countS) {
+        if (countS[c] !== countT[c]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
